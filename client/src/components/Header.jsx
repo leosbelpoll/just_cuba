253c80b20909f5db174a7c197d2660cffc5186ab
ENER <renep2010@gmail.com>
2019-02-07 00:10:00 -0300

    test confict in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,9 @@ import React, { Component, Fragment } from 'react';
 class Header extends Component {
   render() {
     return (
-      <Fragment>
-        <h1>Head_Jost_Cuba</h1>
-      </Fragment>
+      <header>
+        <h1>1Head_Jost_Cuba</h1>
+      </header>
     );
   }
 }
