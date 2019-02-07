394984375a5f59f2a41b509a826fc3b456f06c1a
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2019-02-07 00:04:14 -0300

    Modify Footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React, { Component, Fragment } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <Fragment>
-        <h1>Footer_Jost_Cuba</h1>
-      </Fragment>
+      <footer>
+        <h1>This is the footer</h1>
+      </footer>
     );
   }
 }
