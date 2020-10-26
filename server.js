0cef556aa2d3f09b7771ed09fbe76640189e25f1
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-10-26 15:55:58 -0300

    add more spaces

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(favicon(__dirname + "/client/build/favicon.ico"));
 
-//  the __dirname is the current directory from where the script is running
+//    the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "client/build")));
 
